89677e65ad0eee5182f5b21d3b9e176fad46f465
Cleyton <cleytoodeoliveirarocha@gmail.com>
2023-11-18 13:17:27 -0300

    chore: Adiciona projeto Spring Boot inicial

---
+++ src/test/java/github/com/cleyton_orocha/backendtestjava/BackendTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.cleyton_orocha.backendtestjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
