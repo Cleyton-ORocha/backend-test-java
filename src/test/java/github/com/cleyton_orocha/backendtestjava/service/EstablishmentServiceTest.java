9dc92560a9686272c1a7ccb613891d497ee38568
Cleyton <cleytoodeoliveirarocha@gmail.com>
2023-11-26 10:04:41 -0300

    chore: EstablishmentServiceTest

---
+++ src/test/java/github/com/cleyton_orocha/backendtestjava/service/EstablishmentServiceTest.java
@@ -0,0 +1,11 @@
+package github.com.cleyton_orocha.backendtestjava.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+public class EstablishmentServiceTest {
+    
+}
